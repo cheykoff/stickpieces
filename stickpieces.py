dee19a540703e39b843c8290239fb7569fc568d2
cheykoff <mail@michael-schoelkopf.de>
2020-09-11 11:29:49 +0200

    add chance that any 3 of 4 pieces can form a triangle

--- stickpieces.py
+++ stickpieces.py
@@ -1,8 +1,9 @@
 import random
 
 PIECES = 4
-SAMPLES = 1000
+SAMPLES = 100000
 count = 0
+count2 = 0
 
 def cut_pieces(PIECES):
     pieces = []
@@ -17,9 +18,19 @@ def is_possible():
         return False
     else:
         return True
-    
+
+def all_are_possible():
+    pieces = cut_pieces(PIECES)
+    if pieces[3] >= pieces[1] + pieces[0]:
+        return False
+    else:
+        return True
+   
 for i in range(SAMPLES):
     if is_possible():
         count += 1
+    if all_are_possible():
+        count2 += 1
 
 print(f"Probability to arrange 3 of the 4 pieces in a triangle is {count/SAMPLES}")
+print(f"Probability to be able to arrange any 3 of the 4 pieces into a triangle is {count2/SAMPLES}")
